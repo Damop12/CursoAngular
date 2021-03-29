95ffdbb2a62c240293f40971658ee152e4e4c4ff
damop12 <damop12@gmail.com>
2021-03-29 10:04:36 -0400

    se agrego binding bidireccional [()]

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -10,9 +10,36 @@ export class EmpleadoComponent implements OnInit {
   nombre ='Juan'
   apellido ='Paez'
   edad = 17
- // empresa = 'Ecos'
-llamaEmpresa(value:string){
+  empresa = 'facegoog'
 
+  // cambiaEmpresa(event:Event){//función
+  // this.empresa=(<HTMLInputElement>event.target).value;
+  // }
+
+/*llamaEmpresa(value:string){
+
+}*/
+
+habilitacionCuadro=false;
+
+useRegistrado=false;
+
+textoDeRegistro="No hay nadie Registrado";
+
+getRegistroUsuario(){
+  this.useRegistrado=false;
+}
+
+setusuarioRegistrado(event:Event){
+  
+  //alert('El usuario se acaba de Registrar');
+ //this.textoDeRegistro ="El usuario se acaba de Registrar!"
+
+ if((<HTMLInputElement>event.target).value=="si"){
+
+  this.textoDeRegistro ="El usuario se acaba de Registrar!"
+ }else
+ this.textoDeRegistro ="No hay nadie Registrado";
 }
   constructor() { }
 
